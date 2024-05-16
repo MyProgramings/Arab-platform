41a8549d2f4fc62904f1a6a7e86a42c9e12a269a
MyProgramings <reader1999a@gmail.com>
2024-05-16 05:58:12 +0300

    change Store files and edit gitignore file

--- app/Http/Controllers/HomeworkController.php
+++ app/Http/Controllers/HomeworkController.php
@@ -39,7 +39,7 @@ class HomeworkController extends Controller
         if ($request->hasFile('file_path')) {
             $file = $request->file('file_path');
             $file_path = time() . '.' . $file->getClientOriginalExtension();
-            $file_path = $file->storeAs('file', $file_path);
+            $file_path = $file->storeAs('', $file_path);
         } else
             $file_path = 'No File';
 
